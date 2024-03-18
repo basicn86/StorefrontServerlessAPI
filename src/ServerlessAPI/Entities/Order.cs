2e78d325c974fba9c045b4d0c8e31a0229984480
Nedim Basic <basicn86@gmail.com>
2024-03-18 17:53:09 -0400

    Added order table

---
+++ src/ServerlessAPI/Entities/Order.cs
@@ -0,0 +1,23 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+
+namespace ServerlessAPI.Entities;
+
+// <summary>
+/// Map the Book Class to DynamoDb Table
+/// To learn more visit https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/DeclarativeTagsList.html
+/// </summary>
+[DynamoDBTable("StorefrontSAMOrders")]
+public class Order
+{
+    ///<summary>
+    /// Map c# types to DynamoDb Columns 
+    /// to learn more visit https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/MidLevelAPILimitations.SupportedTypes.html
+    /// <summary>
+    [DynamoDBHashKey] //Partition key
+    public int Id { get; set; } = 0;
+
+    [DynamoDBProperty]
+    public string Name { get; set; } = string.Empty;
+    [DynamoDBProperty]
+    public decimal Price { get; set; } = 0;
+}
